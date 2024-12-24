62b8c87d964280e1c5091d6dea48ca6cf6fa9250
ramanjaneyulu17 <ramanjaneyulu17.m@gmail.com>
2024-12-24 16:34:28 +0530

    first edit

--- src/main/java/com/example/JobApp/model/JobPost.java
+++ src/main/java/com/example/JobApp/model/JobPost.java
@@ -11,7 +11,7 @@ import java.util.List;
 //@Data
 //@NoArgsConstructor
 //@AllArgsConstructor
-//@Component
+@Component
 public class JobPost {
     private int postId;
     private String postProfile;
