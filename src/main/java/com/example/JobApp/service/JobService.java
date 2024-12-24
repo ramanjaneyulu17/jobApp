99e313402dabce99611d2146434385be21dcb847
ramanjaneyulu17 <ramanjaneyulu17.m@gmail.com>
2024-12-24 16:22:59 +0530

    first commit

---
+++ src/main/java/com/example/JobApp/service/JobService.java
@@ -0,0 +1,22 @@
+package com.example.JobApp.service;
+
+import com.example.JobApp.model.JobPost;
+import com.example.JobApp.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepo repo;
+
+    public void addJob(JobPost jobPost){
+       repo.addJob(jobPost);
+    }
+    public List<JobPost> viewalljobs(){
+       return  repo.getAllJobs();
+    }
+}
